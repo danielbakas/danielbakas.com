7fa9a4a9495bc8645b21d470837a7bedddd14500
Daniel B <danielbakas@gmail.com>
2022-11-30 16:59:02 -0600

    Add new build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {Form} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('Si yo digo ping tu dices...')
+    const handleChange = e => {
+        setValue(e.target.value)
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>AO1657103 | Daniel Bakas Amuchástegui</p>
+                <h1 className="pb-5">Si yo digo ping tu dices...</h1>
+                <img src="logo.svg" className="App-logo" alt="logo"/>
+                <Form className="pt-5">
+                    <Form.Group className="mb-3">
+                        <Form.Control required placeholder="E.g. Pong"
+                                      onChange={handleChange}/>
+                    </Form.Group>
+                </Form>
+                {value && <p className="pt-2">
+                    Pong!! 🥳... O bueno también puede ser {value} 😋
+                </p>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
